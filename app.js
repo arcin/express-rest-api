48fa4f0416a820b782d6b7f4b5dba38cf1caa2a8
Charles Arciniega <charlesarcin@gmail.com>
2014-06-23 12:16:08 -0700

    creates get route and listens on port 4700

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 var express = require('express'),
     app = express();
 
+app.get('/', function(req, res){
+  res.type('text/plain');
+  res.send('This is a response from an express server!');
+});
+
+app.listen(4700);
