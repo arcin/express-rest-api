0bec7740ee60c74a55811d7f092e88d4847db33c
Charles Arciniega <charlesarcin@gmail.com>
2014-06-23 17:19:01 -0700

    Post Route     * Adds body-parser and makes the app use it.         - I was getting an error with express.bodyParser();     * creates a post route at '/quote'     * checks to see if incoming json has an author and text         - returns 404 if it doesn't.     * creates a newQuote object if the data is valid.     * adds the newQuote to the array and returns a true message in json

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
     app = express();
+
+app.use(bodyParser.json());
 
 var quotes = [
   { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
@@ -29,5 +32,25 @@ app.get('/quote/:id', function(req, res){
   }
   res.json(quote);
 });
+
+app.post('/quote', function(req, res){
+  var bodyLacksAuthor = req.body.hasOwnProperty('author'),
+      bodyLacksText = req.body.hasOwnProperty('text'),
+      newQuote;
+
+  if (bodyLacksAuthor || bodyLacksText) {
+    res.statusCode = 400;
+    res.send('400: You must include both an author and text');
+  }
+
+  newQuote = {
+    author: req.body.author,
+    text: req.body.text
+  };
+  quotes.push(newQuote);
+  res.json(true);
+});
+
+
 
 app.listen(4700);
